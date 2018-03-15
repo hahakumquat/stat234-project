bc567042ca78a8c107ef6f60d31615e769cafcc8
Michael Ge <michaelge@college.harvard.edu>
2018-03-15 16:50:04 -0400

    DQN

---
+++ cartpole/main.py
@@ -0,0 +1,10 @@
+import gym
+import math
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+# from itertools import count
+# from copy import deepcopy
+# from PIL import Image
+
+env = gym.make('CartPole-v0').unwrapped
