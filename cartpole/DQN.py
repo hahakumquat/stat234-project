7da791c792386cba1ab937d12d6a7120f0d431dd
Michael Ge <michaelge@college.harvard.edu>
2018-03-16 17:41:35 -0400

    wip

---
+++ cartpole/DQN.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torchvision.transforms as T
+
+from PIL import Image
+
+class DQN(nn.Module):
+
+    def __init__(self, env, screen_width=600, batch_sz=128, lr=0.1):
+        super(DQN, self).__init__()
+
+        ## DQN architecture
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu1 = nn.LeakyReLU(0.05)
+        
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.relu2 = nn.LeakyReLU(0.05)
+        
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(32)
+        self.relu3 = nn.LeakyReLU(0.05)
+        
+        self.out_layer = nn.Linear(448, 2)
+
+        self.env = env
+        self.screen_width = 600
+        self.batch_size = batch_sz
+        self.learning_rate = lr
+        
+        self.optimizer = optim.RMSprop(self.parameters(),
+                                       lr=self.learning_rate)
+        self.lossFunction = nn.SmoothL1Loss
+
+    def forward(self, ):
+        states = self.relu1(self.bn1(self.conv1(states)))
+        states = self.relu2(self.bn2(self.conv2(states)))
+        states = self.relu3(self.bn3(self.conv3(states)))
+        return self.out_layer(states.view(states.size(0), -1))
+
+    def train(self, memory):
+        state_action_values = self.forward(states)
+        loss = self.lossFunction(state_action_values, expected_state_action_values)
+        self.optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print(loss.data)
+
+    # helper functions
+    def get_cart_location(self):
+        world_width = self.env.x_threshold * 2
+        scale = self.screen_width / world_width
+        return int(env.state[0] * scale + self.screen_width / 2.0)  # MIDDLE OF CART
+
+    def get_screen(self):
+        resize = T.Compose([T.ToPILImage(),
+                    T.Scale(40, interpolation=Image.CUBIC),
+                    T.ToTensor()])
+        
+        screen = self.env.render(mode='rgb_array').transpose(
+            (2, 0, 1))  # transpose into torch order (CHW)
+        # Strip off the top and bottom of the screen
+        screen = screen[:, 160:320]
+        view_width = 320 # can be like 60
+        cart_location = get_cart_location()
+        if cart_location < view_width // 2:
+            slice_range = slice(view_width)
+        elif cart_location > (self.screen_width - view_width // 2):
+            slice_range = slice(-view_width, None)
+        else:
+            slice_range = slice(cart_location - view_width // 2,
+                                cart_location + view_width // 2)
+        # Strip off the edges, so that we have a square image centered on a cart
+        screen = screen[:, :, slice_range]
+        # Convert to float, rescale, convert to torch tensor
+        # (this doesn't require a copy)
+        screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+        screen = torch.from_numpy(screen)
+        # Resize, and add a batch dimension (BCHW)
+        # output 1x3x40x80
+        print(resize(screen).unsqueeze(0).type(Tensor).shape)
+        return resize(screen).unsqueeze(0).type(Tensor)
+
+
--- cartpole/main.py
+++ cartpole/main.py
@@ -1,10 +1,90 @@
+import sys
+import os
 import gym
 import math
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-# from itertools import count
+import torch
+
+# models
+from DQN import DQN
+
+# agents
+
+from itertools import count
 # from copy import deepcopy
 # from PIL import Image
 
+#first change the cwd to the script path
+scriptPath = os.path.realpath(os.path.dirname(sys.argv[0]))
+os.chdir(scriptPath)
+
+#append the relative location you want to import from
+sys.path.append("../utils")
+
+#import your module stored in '../common'
+from ReplayMemory import ReplayMemory, Transition
+
+memory = ReplayMemory(1000)
+
 env = gym.make('CartPole-v0').unwrapped
+model = None
+agent = None
+
+if len(sys.argv) == 3:
+    model_name = sys.argv[1]
+    agent_name = sys.argv[2]
+    if model_name == 'DQN':
+        model = DQN(env)
+    else:
+        raise Exception('Model does not exist. Ex: For DQN.py, use DQN')
+    if agent_name == 'EpsilonGreedy':
+        agent = EpsilonGreedy(model)
+    else:
+        print("TODO")
+        # raise Exception('Agent does not exist. Ex: For EpsilonGreedy.py, use EpsilonGreedy')
+else:
+    raise Exception("Usage: python main.py <model_name> <agent_name>")
+
+BATCH_SIZE = 128
+num_episodes = 1000
+for i_episode in range(num_episodes):
+    # Initialize the environment and state
+    env.reset()
+    last_screen = env.render(mode='rgb_array')
+    current_screen = env.render(mode='rgb_array')
+    state = current_screen - last_screen
+    for t in count():
+        # Select and perform an action
+        # action = agent.select_action(state)
+        action = env.action_space.sample()
+        _, reward, done, _ =  env.step(action) # env.step(action[0, 0])
+        reward = torch.FloatTensor([reward])
+
+        # Observe new state
+        last_screen = current_screen
+        current_screen = env.render(mode='rgb_array')
+        if not done:
+            next_state = current_screen - last_screen
+        else:
+            next_state = None
+
+        # Store the transition in memory
+        memory.push(state, action, next_state, reward)
+
+        # Move to the next state
+        state = next_state
+
+        # Perform one step of the optimization (on the target network)
+        if len(memory) >= BATCH_SIZE:
+            transitions = memory.sample(BATCH_SIZE)
+            batch = Transition(*zip(*transitions))
+            model.train(transitions)
+            
+        if done:
+            episode_durations.append(t + 1)
+            # plot_durations()
+            break
+
+print('Complete')
