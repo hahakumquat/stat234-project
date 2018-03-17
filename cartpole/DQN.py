9006b3f9ea56ad3f3b4ada489de19e6902152298
Michael Ge <michaelge@college.harvard.edu>
2018-03-17 16:12:30 -0400

    IT WORKS

--- cartpole/DQN.py
+++ cartpole/DQN.py
@@ -3,9 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.autograd import Variable
-import torchvision.transforms as T
 
-from PIL import Image
 
 class DQN(nn.Module):
 
@@ -38,7 +36,6 @@ class DQN(nn.Module):
         self.loss_function = nn.SmoothL1Loss
 
     def forward(self, state_batch):
-        state_batch = self.truncate(state_batch)
         state_batch = self.relu1(self.bn1(self.conv1(state_batch)))
         state_batch = self.relu2(self.bn2(self.conv2(state_batch)))
         state_batch = self.relu3(self.bn3(self.conv3(state_batch)))
@@ -50,10 +47,7 @@ class DQN(nn.Module):
         # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
         # columns of actions taken
         state_action_values = self.forward(state_batch)
-        print(state_action_values.shape)
-        print(action_batch.data)
-        action_one_hots = Variable(torch.zeros(len(action_batch), 2).scatter_(1, action_batch.data.unsqueeze(1), 1).type(torch.LongTensor))
-        state_action_values = state_action_values.gather(1, action_one_hots)
+        state_action_values = state_action_values.gather(1, action_batch)
 
         # Compute the expected Q values
         expected_state_action_values = (next_state_values * self.gamma) + reward_batch
@@ -75,40 +69,3 @@ class DQN(nn.Module):
         # loss.backward()
         # optimizer.step()
         # print(loss.data)
-
-    def truncate(self, state_batch):
-        return Variable(torch.cat([self.cut_screen(screen) for screen in state_batch]))
-
-    # helper functions
-    def get_cart_location(self):
-        world_width = self.env.x_threshold * 2
-        scale = self.screen_width / world_width
-        return int(env.state[0] * scale + self.screen_width / 2.0)  # MIDDLE OF CART
-
-    def resize(self, screen):
-        rsz = T.Compose([T.ToPILImage(),
-                T.Resize(40, interpolation=Image.CUBIC),
-                T.ToTensor()])
-        return rsz(screen)
-        
-    def cut_screen(self, screen):
-        screen = screen.permute((2, 0, 1))  # transpose into torch order (CHW)
-        # Strip off the top and bottom of the screen
-        screen = screen[:, 160:320]
-        view_width = 320 # can be like 60
-        # cart_location = get_cart_location()
-        # if cart_location < view_width // 2:
-        #     slice_range = slice(view_width)
-        # elif cart_location > (self.screen_width - view_width // 2):
-        #     slice_range = slice(-view_width, None)
-        # else:
-        #     slice_range = slice(cart_location - view_width // 2,
-        #                         cart_location + view_width // 2)
-        # Strip off the edges, so that we have a square image centered on a cart
-        # screen = screen[:, :, slice_range]
-        # Convert to float, rescale, convert to torch tensor
-        # (this doesn't require a copy)
-        screen = screen / 255
-        # Resize, and add a batch dimension (BCHW)
-        # output 1x3x40x80
-        return self.resize(screen.data).unsqueeze(0).type(torch.FloatTensor)
--- cartpole/main.py
+++ cartpole/main.py
@@ -7,6 +7,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 import torch
 from torch.autograd import Variable
+import torchvision.transforms as T
+from PIL import Image
 
 # models
 from DQN import DQN
@@ -15,8 +17,6 @@ from DQN import DQN
 
 from itertools import count
 # from copy import deepcopy
-# from PIL import Image
-
 #first change the cwd to the script path
 scriptPath = os.path.realpath(os.path.dirname(sys.argv[0]))
 os.chdir(scriptPath)
@@ -49,72 +49,93 @@ if len(sys.argv) == 3:
 else:
     raise Exception("Usage: python main.py <model_name> <agent_name>")
 
+def main(batch_sz, num_episodes):
+    for i_episode in range(num_episodes):
+
+        # Initialize the environment and state
+        env.reset()
+        last_screen = get_screen(env)
+        current_screen = get_screen(env)
+        state = current_screen - last_screen
+        state_info = env.state
+        for t in count():
+            # Select and perform an action
+            action = select_action(state)
+            _, reward, done, _ =  env.step(action[0, 0])
+            reward = torch.FloatTensor([reward])
+
+            # Observe new state
+            last_screen = current_screen
+            current_screen = get_screen(env)
+            if not done:
+                next_state = current_screen - last_screen
+
+                # get OpenAI Gym's 4 state elements
+                next_state_info = env.state
+            else:
+                next_state = None
+                next_state_info = None
+
+            # Store the transition in memory
+            memory.push(state, action, reward, next_state, state_info, next_state_info)
+
+            # Move to the next state
+            state = next_state
+            state_info = next_state_info
+
+            # Perform one step of the optimization (on the target network)
+            if len(memory) >= BATCH_SIZE:
+                transitions = memory.sample(BATCH_SIZE)
+                # stackoverflow: 
+                batch = Transition(*zip(*transitions))
+
+                # Compute a mask of non-final states and concatenate the batch elements
+                non_final_mask = torch.ByteTensor(tuple(map(lambda s: s is not None,
+                                                      batch.next_state)))
+
+                # We don't want to backprop through the expected action values and volatile
+                # will save us on temporarily changing the model parameters'
+                # requires_grad to False!
+                non_final_next_states = Variable(torch.cat([s for s in batch.next_state if s is not None]), volatile=True)
+                state_batch = Variable(torch.cat(batch.state))
+                action_batch = Variable(torch.cat(batch.action))
+                reward_batch = Variable(torch.cat(batch.reward))
+                # Compute V(s_{t+1}) for all next states.
+                next_state_values = Variable(torch.zeros(len(state_batch)).type(torch.FloatTensor))
+                next_state_values[non_final_mask] = model.forward(non_final_next_states).max(1)[0]
+                next_state_values.volatile = False
+
+                # Now, we don't want to mess up the loss with a volatile flag, so let's
+                # clear it. After this, we'll just end up with a Variable that has
+                # requires_grad=False
+                # next_state_values.volatile = False
+
+                model.train(state_batch, action_batch, reward_batch, next_state_values)
+
+            if done:
+                episode_durations.append(t + 1)
+                # plot_durations()
+                break
+
+def select_action(state):
+    return torch.LongTensor([[0]])
+
+def get_screen(env):
+    screen = env.render(mode='rgb_array').transpose((2, 0, 1))
+
+    # Strip off the top and bottom of the screen
+    screen = screen[:, 160:320]
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+    # Resize, and add a batch dimension (BCHW)
+    return resize(screen).unsqueeze(0).type(torch.FloatTensor)
+
+def resize(screen):
+    rsz = T.Compose([T.ToPILImage(),
+            T.Resize(40, interpolation=Image.CUBIC),
+            T.ToTensor()])
+    return rsz(screen)
+
 BATCH_SIZE = 10
 num_episodes = 1000
-for i_episode in range(num_episodes):
-
-    # Initialize the environment and state
-    env.reset()
-    last_screen = env.render(mode='rgb_array')
-    current_screen = env.render(mode='rgb_array')
-    state = current_screen - last_screen
-    state_info = env.state
-    for t in count():
-        # Select and perform an action
-        # action = agent.select_action(state)
-        action = env.action_space.sample()
-        _, reward, done, _ =  env.step(action) # env.step(action[0, 0])
-
-        # Observe new state
-        last_screen = current_screen
-        current_screen = env.render(mode='rgb_array')
-        if not done:
-            next_state = current_screen - last_screen
-
-            # get OpenAI Gym's 4 state elements
-            next_state_info = env.state
-        else:
-            next_state = None
-            next_state_info = None
-
-        # Store the transition in memory
-        memory.push(state, action, reward, next_state, state_info, next_state_info)
-
-        # Move to the next state
-        state = next_state
-        state_info = next_state_info
-
-        # Perform one step of the optimization (on the target network)
-        if len(memory) >= BATCH_SIZE:
-            transitions = memory.sample(BATCH_SIZE)
-            # stackoverflow: 
-            batch = Transition(*zip(*transitions))
-                
-            # Compute a mask of non-final states and concatenate the batch elements
-            non_final_mask = torch.ByteTensor(tuple(map(lambda s: s is not None,
-                                                  batch.next_state)))
-
-            # We don't want to backprop through the expected action values and volatile
-            # will save us on temporarily changing the model parameters'
-            # requires_grad to False!
-            non_final_next_states = Variable(torch.from_numpy(np.array([s for s in batch.next_state if s is not None])))
-            state_batch = Variable(torch.from_numpy(np.array(batch.state)))
-            action_batch = Variable(torch.LongTensor(batch.action))
-            reward_batch = Variable(torch.FloatTensor(batch.reward))
-            # Compute V(s_{t+1}) for all next states.
-            next_state_values = Variable(torch.zeros(len(state_batch)).type(torch.FloatTensor))
-            next_state_values[non_final_mask] = model.forward(non_final_next_states).max(1)[0]
-
-            # Now, we don't want to mess up the loss with a volatile flag, so let's
-            # clear it. After this, we'll just end up with a Variable that has
-            # requires_grad=False
-            # next_state_values.volatile = False
-
-            model.train(state_batch, action_batch, reward_batch, next_state_values)
-            
-        if done:
-            episode_durations.append(t + 1)
-            # plot_durations()
-            break
-
-print('Complete')
+main(BATCH_SIZE, num_episodes)
