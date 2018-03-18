found bug in epsilon calculation

cartpole/DQN_GS.py
@@ -66,7 +66,7 @@ class DQNGS(nn.Module):
             param.grad.data.clamp_(-1, 1)
         self.optimizer.step()
         
-        if len(self.losses) % 10 == 0:
+        if len(self.losses) % 100 == 0:
             self.plot_losses()
 
     def plot_losses(self):

cartpole/EpsilonGreedy.py
@@ -16,7 +16,7 @@ class EpsilonGreedy():
 
     def select_action(self, state):
         sample = random.random()
-        threshold = self.eps_end + (self.eps_start - self.eps_end) * np.exp(-1 * self.steps_done) / self.eps_decay
+        threshold = self.eps_end + (self.eps_start - self.eps_end) * np.exp(-1 * self.steps_done / self.eps_decay)
         self.steps_done += 1
         if sample > threshold:
             return self.model.forward(Variable(state, volatile=True)).data.max(1)[1].view(1, 1)