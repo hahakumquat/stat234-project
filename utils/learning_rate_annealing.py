414e0a7b28349f8e8e3c77793d88b53ffb361655
rlouyang <richardlouyang@gmail.com>
2018-03-20 19:27:09 -0400

    more lr annealing + clean NoTraining

--- utils/learning_rate_annealing.py
+++ utils/learning_rate_annealing.py
@@ -1,6 +1,7 @@
 import numpy as np
+import torch.optim as optim
 
 learning_rate_annealer = lambda epoch: max(np.exp(-epoch / 8 - 2), 0.00025)
-scheduler = LambdaLR(optimizer, 
+scheduler = optim.LambdaLR(optimizer, 
                      lr_lambda=learning_rate_annealer)
 scheduler.step()
