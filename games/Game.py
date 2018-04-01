ea1e360b38937eb27ecb3fc2ecbbe847e4da7d8d
Richard Ouyang <richardlouyang@gmail.com>
2018-03-31 20:51:31 -0400

    remove check for env.state

--- games/Game.py
+++ games/Game.py
@@ -5,11 +5,6 @@ class Game():
     def __init__(self, game_name):
         self.env = gym.make(game_name).unwrapped
         self.file_prefix = game_name.split('-')[0] + '_'
-        try: # atari games don't have a state attribute
-            gym.envs.atari
-            self.env.state = None
-        except AttributeError:
-            pass
 
     def modify_screen(self, screen):
         return screen
