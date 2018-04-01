c58f534b22d56d88d55f57412f68a54fa37095d7
rlouyang <richardlouyang@gmail.com>
2018-04-01 13:25:18 -0400

    add PCA for saved states

--- utils/PCA.py
+++ utils/PCA.py
@@ -1,19 +1,28 @@
 import numpy as np
-from sklearn.decomposition import IncrementalPCA as sklPCA
+from sklearn.decomposition import PCA as sklPCA
+from sklearn.preprocessing import StandardScaler
 
 class PCA():
 
-    def __init__(self, states, n=100, batch_size=10):
-        self.pca = sklPCA(n_components=n, batch_size=10)
-        X = states.reshape((len(states), -1))
-        self.original_dim = states[0].shape
-        self.pca.fit(X)
+    def __init__(self, states_file, original_dim=(80, 80), n=100):
+        self.original_dim = original_dim
+
+        states = np.genfromtxt(states_file, delimiter=',')
+        states /= 255 # states_file is made up of ints
+
+        self.std_scale = StandardScaler().fit(states)
+        states_std = self.std_scale.transform(states)
+
+        self.pca = sklPCA(n_components=n)
+        self.pca.fit(states_std)
 
     def transform(self, state):
-        # 10 x 1
         y = state.reshape(len(state), -1)
-        return self.pca.transform(y)
+        y_std = self.std_scale.transform(y)
+        return self.pca.transform(y_std)
         
     def inv_transform(self, transformed):
-        return self.pca.inverse_transform(transformed).reshape((len(transformed), self.original_dim[0], self.original_dim[1]))
+        return self.std_scale.inverse_transform(
+            self.pca.inverse_transform(transformed)).reshape(
+            (len(transformed), self.original_dim[0], self.original_dim[1]))
 
