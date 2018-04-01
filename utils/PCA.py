b9cd78ceef78120e3cd3b5936a3837c1ce9bb6f9
Michael Ge <michaelge@college.harvard.edu>
2018-03-31 21:43:06 -0400

    PCA

---
+++ utils/PCA.py
@@ -0,0 +1,19 @@
+import numpy as np
+from sklearn.decomposition import PCA as sklPCA
+
+class PCA():
+
+    def __init__(self, states, n = 100):
+        self.pca = sklPCA(n_components = n)
+        X = states.reshape((len(states), -1))
+        self.original_dim = states[0].shape
+        self.pca.fit(X)
+
+    def transform(self, state):
+        # 10 x 1
+        y = state.reshape(len(state),-1)
+        return self.pca.transform(y)
+        
+    def inv_transform(self, transformed):
+        return self.pca.inverse_transform(transformed).reshape((len(transformed), self.original_dim[0], self.original_dim[1]))
+
