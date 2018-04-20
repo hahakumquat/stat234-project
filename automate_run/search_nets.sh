16ce22b86cbd3d40d4bd29a22d5e44332f1ded2c
Richard Ouyang <richardlouyang@gmail.com>
2018-04-20 15:01:15 -0400

    search nets update again

--- automate_run/search_nets.sh
+++ automate_run/search_nets.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 games=(CartPole-v0 Acrobot-v1 MountainCar-v0)
-# params=(128 64 128_64 128_64_32 128_128_64_32)
-params=(16 16_32 16_32_64 64_32_16 64_16 64 32 32_32_16)
+# params=()
+params=(16 16_32 16_32_64 64_32_16 64_16 64 32 32_32_16 128 128_64 128_64_32 128_128_64_32)
 models=(DQN_PCA DDQN_PCA)
 for g in "${games[@]}"
 do
